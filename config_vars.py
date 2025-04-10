f54566ab9388207bb2fdf70fc72c1a955a0f7ef7
drobins <daniel.robins@kunan.com.ar>
2025-04-10 08:48:29 -0300

    update 2025

--- config_vars.py
+++ config_vars.py
@@ -1,4 +1,4 @@
 #BBDD_CONNECTION = "postgresql://{user}:{password}@{host}/{db_name}"  
 
 import cx_Oracle
-BBDD_CONNECTION = "oracle+cx_oracle://hca:2712adaD.oracle@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))"
+BBDD_CONNECTION = "oracle+cx_oracle://hca:hca@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))"
