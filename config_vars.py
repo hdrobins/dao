0e065badcf30ff22cd8e482d0673c715ff47fe57
drobins <daniel.robins@kunan.com.ar>
2025-04-10 08:09:30 -0300

    New version with oracle database in docker

--- config_vars.py
+++ config_vars.py
@@ -1,11 +1,4 @@
-#BBDD_CONNECTION = "postgresql://admin:ueDrxqVQwtQeBGJ@192.168.88.247/rasa"  
-
-
-BBDD_CONNECTION = "oracle+cx_oracle://hca:{password}@drobinsundec_medium/?encoding=UTF-8&nencoding=UTF-8"
+#BBDD_CONNECTION = "postgresql://{user}:{password}@{host}/{db_name}"  
 
 import cx_Oracle
-
-d="/opt/oracle/instantclient_19_20"
-print(d)
-cx_Oracle.init_oracle_client(lib_dir=d)
-
+BBDD_CONNECTION = "oracle+cx_oracle://hca:2712adaD.oracle@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))"
